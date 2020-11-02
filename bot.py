840b86f958164b682e69b6d74a158da87e13dd7b
emrecoskun705 <emrecoskun705@gmail.com>
2020-11-02 21:22:40 +0300

    delete messages

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
 import os
 
 
 
 TOKEN = os.environ['BOT_TOKEN']
 ######
-client = discord.Client()
+Client = discord.Client()
+client =commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
 @client.event
 async def on_message(message):
 
@@ -17,6 +22,25 @@ async def on_message(message):
         return
     if message.content.startswith('-twitch'):
         await message.channel.send('https://www.twitch.tv/jaekiner')
+
+    if message.content.startswith('-sil'):
+        data = message.content.split()
+        count = 6
+        if len(data) > 1:
+            try:
+                count = int(data[1]) + 1
+                if(int(data[1]) > 21):
+                    await message.channel.send('En fazla 20 mesajı bir anda silebilirsin!')
+                    return
+            except:
+                await message.channel.send('Örnek: "-sil" veya "-sil 10"')
+                return
+        
+        await message.channel.purge(limit=count)
+    
+
+
+
 
 
 client.run(TOKEN)
