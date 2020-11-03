8544075e68ba3bbe7eecbf74b50ddc7f3754c893
emrecoskun705 <emrecoskun705@gmail.com>
2020-11-03 03:06:35 +0300

    add initial help

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import asyncio
 import os
 
 
-
 TOKEN = os.environ['BOT_TOKEN']
 ######
 Client = discord.Client()
@@ -20,9 +19,11 @@ async def on_message(message):
 
     if message.author == client.user:
         return
+    #-twitch
     if message.content.startswith('-twitch'):
         await message.channel.send('https://www.twitch.tv/jaekiner')
 
+    #-sil
     if message.content.startswith('-sil'):
         data = message.content.split()
         count = 6
@@ -30,13 +31,28 @@ async def on_message(message):
             try:
                 count = int(data[1]) + 1
                 if(int(data[1]) > 21):
-                    await message.channel.send('En fazla 20 mesajı bir anda silebilirsin!')
+                    await message.channel.send(colour('En fazla 20 mesajı bir anda silebilirsin!'))
                     return
             except:
                 await message.channel.send('Örnek: "-sil" veya "-sil 10"')
                 return
         
         await message.channel.purge(limit=count)
+
+    #-help
+    if message.content.startswith('-help'):
+        await message.channel.send(helpMessage())
+
+def colour(comment):
+    comment = "```css\n" + comment + "```"
+    return comment
+
+def helpMessage():
+    string = f"""
+        {colour('-twitch')} {colour('-sil')}
+    """
+    return string
+        
     
 
 
