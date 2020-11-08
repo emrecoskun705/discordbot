19041d43c633e03ad6d8ede1ea565abeacd25cf0
emrecoskun705 <emrecoskun705@gmail.com>
2020-11-08 19:33:32 +0300

    remove -delte command

--- bot.py
+++ bot.py
@@ -1,47 +1,73 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
+import youtube_dl
 import os
 
 
 TOKEN = os.environ['BOT_TOKEN']
 ######
-Client = discord.Client()
-client =commands.Bot(command_prefix='.')
+bot =commands.Bot(command_prefix='-')
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+players = {}
+
+@bot.command()
+async def twitch(ctx):
+    await ctx.send('https://www.twitch.tv/jaekiner')
     
-@client.event
-async def on_message(message):
 
-    if message.author == client.user:
-        return
+
+@bot.command(aliases=['join'])
+async def join_voice(ctx):
+    channel = ctx.author.voice.channel
+    if channel:
+        await channel.connect()
+
+@bot.command(aliases=['leave'])
+async def leave_voice(ctx):
+    server = ctx.message.guild.voice_client
+    if server:
+        await server.disconnect()
+
+
+
+
+
+
+
+    
+
+
+
+    
+#@bot.event
+#def ondsfsd_message(message):
+
+ #   if message.author == bot.user:
+  #      return
     #-twitch
-    if message.content.startswith('-twitch'):
-        await message.channel.send('https://www.twitch.tv/jaekiner')
+    #if message.content.startswith('-twitch'):
+     #   await message.channel.send('https://www.twitch.tv/jaekiner')
 
     #-sil
-    if message.content.startswith('-sil'):
-        data = message.content.split()
-        count = 6
-        if len(data) > 1:
-            try:
-                count = int(data[1]) + 1
-                if(int(data[1]) > 21):
-                    await message.channel.send(colour('En fazla 20 mesajı bir anda silebilirsin!'))
-                    return
-            except:
-                await message.channel.send('Örnek: "-sil" veya "-sil 10"')
-                return
+   # if message.content.startswith('-sil'):
+    #    data = message.content.split()
+     #   count = 6
+      #  if len(data) > 1:
+       #     try:
+        #        count = int(data[1]) + 1
+         #       if(int(data[1]) > 21):
+                    #await message.channel.send(colour('En fazla 20 mesajı bir anda silebilirsin!'))
+          #          return
+           # except:
+                #await message.channel.send('Örnek: "-sil" veya "-sil 10"')
+            #    return
         
-        await message.channel.purge(limit=count)
+       # await message.channel.purge(limit=count)
 
     #-help
-    if message.content.startswith('-help'):
-        await message.channel.send(helpMessage())
+    #if message.content.startswith('-help'):
+        #await message.channel.send(helpMessage())
 
 def colour(comment):
     comment = "```css\n" + comment + "```"
@@ -59,4 +85,4 @@ def helpMessage():
 
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
